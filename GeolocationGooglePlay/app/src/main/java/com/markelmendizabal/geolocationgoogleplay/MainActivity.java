4ced5c535a2d2d92c469d3d40e5f74fd0595ae1d
markelor <mendimarkel@gmail.com>
2015-04-08 11:34:21 +0200

    geolocation con google play services

--- GeolocationGooglePlay/app/src/main/java/com/markelmendizabal/geolocationgoogleplay/MainActivity.java
+++ GeolocationGooglePlay/app/src/main/java/com/markelmendizabal/geolocationgoogleplay/MainActivity.java
@@ -1,44 +1,97 @@
 package com.markelmendizabal.geolocationgoogleplay;
 
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 
-public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-private GoogleApiClient client;
+public class MainActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private LocationRequest mLocationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        client = new GoogleApiClient.Builder(this)
-                .addApi(Drive.API)
-                .addScope(Drive.SCOPE_FILE)
+        //nuestro cliente
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
                 .build();
+
+        //crear el objeto de nuestra solicitud de geolocation
+        mLocationRequest=LocationRequest.create()
+                .setPriority(LocationRequest
+                .PRIORITY_HIGH_ACCURACY)
+                .setInterval(10*1000)
+                .setFastestInterval(1*1000);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+            mGoogleApiClient.disconnect();
+        }
+    }
 
     @Override
     public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location==null){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
+
+        }else{
+            handleNewLocation(location);
+        }
 
     }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+    }
+
 
     @Override
     public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended");
 
     }
+
+
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
 }
+
+
+
